21d4524b5480106877f97debf19af11784e0e11b
Pau Fernández <pau.fernandez@upc.edu>
2012-04-06 20:02:33 +0200

    Build script mejorado

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 path=$(echo $GOPATH | cut -d: -f1)
-gcc -Wall -static -o $path/bin/grz-jail grz-jail/grz-jail.c
-go install ./grz
-go install ./grz-eval
-go install ./grz-judge
+echo -n "grz";        go install ./grz
+echo -n " grz-jail";  gcc -Wall -static -o $path/bin/grz-jail grz-jail/grz-jail.c
+echo -n " grz-eval";  go install ./grz-eval
+echo -n " grz-judge"; go install ./grz-judge
+echo
