a00f8cbd2441a177bea3563943f3b29e84ffb805
Pau Fernández <pau.fernandez@upc.edu>
2012-04-19 16:57:46 +0200

    build.sh: Pequeña simplificación

--- build.sh
+++ build.sh
@@ -3,5 +3,4 @@ echo -n "grz-jail";   go install ./grz-jail
 echo -n " grz";       go install ./grz
 echo -n " grz-db";    go install ./grz-db
 echo -n " grz-eval";  go install ./grz-eval
-echo -n " grz-judge"; go install ./grz-judge
-echo
+echo    " grz-judge"; go install ./grz-judge
