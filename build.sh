0ea5c9556319801d15278a3cc330121030c1e069
Pau Fernández <pau.fernandez@upc.edu>
2012-04-19 16:51:43 +0200

    Cambio el script 'build.sh' para reflejar el cambio en 'grz-jail'

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-path=$(echo $GOPATH | cut -d: -f1)
-echo -n "grz-jail";   gcc -Wall -static -o $path/bin/grz-jail grz-eval/grz-jail/grz-jail.c
+echo -n "grz-jail";   go install ./grz-jail
 echo -n " grz";       go install ./grz
 echo -n " grz-db";    go install ./grz-db
 echo -n " grz-eval";  go install ./grz-eval
